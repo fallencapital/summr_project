f797d0a8d8376dbd140b1f733541ecd8726ab394
dell conagher <twitchstreamerog@gmail.com>
2025-08-23 18:20:48 +0400

    final version

--- modules/search_answer/sanda_commands.py
+++ modules/search_answer/sanda_commands.py
@@ -1,1 +1,31 @@
+import sys
+from pathlib import Path
+import asyncio
 
+current_file = Path(__file__).resolve()
+invincible = current_file.parent.parent.parent
+sys.path.insert(0, str(invincible))
+
+from modules.search_answer.auto_browser import AutoBrowser
+
+class SandAcommands:
+    @staticmethod
+    def sanadcommands(core):
+
+        def autobrowse():
+            print("Введите поисковый запрос: ")
+            search_query = input().strip()
+            
+            if not search_query:
+                print("Запрос не может быть пустым!")
+                return
+            
+            print(f"Выполняю поиск по запросу: '{search_query}'...")
+            
+            asyncio.run(AutoBrowser.wakeup(search_query))
+            
+            print("Поиск завершен. Результаты сохранены в файл itog.txt")
+        core.register_command("браузер", autobrowse)
+        core.register_command("поиск", autobrowse) 
+        
+        return core 
