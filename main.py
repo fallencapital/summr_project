ed7e0646f786916d47ea0e2d6bee828fd286e3dd
dell conagher <twitchstreamerog@gmail.com>
2025-08-23 18:19:55 +0400

    final version

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import sys
 from pathlib import Path
 from modules.system.system_command import SystemCommands
+from modules.search_answer.sanda_commands import SandAcommands
+from modules.files.file_commands import FileCommands
+from core.kernel import Core 
 
 def main():
-
     current_file = Path(__file__).resolve()
     sys.path.insert(0, str(current_file.parent.parent.parent))
 
     print("PC Ассистент запущен (введите 'help' для списка команд)")
-    system = SystemCommands.systemcommands()
 
+    core = Core()
+
+    SystemCommands.systemcommands(core)
+    SandAcommands.sanadcommands(core) 
+    FileCommands.filecommands(core)
 
     while True:
         try:
@@ -21,25 +27,32 @@ def main():
             
             if user_input in ('help', 'помощь'):
                 print("\nДоступные команды:")
-                print(" - процессы (список процессов)")
-                print(" - cpu (загрузка процессора)")
-                print(" - ram (использование памяти)")
-                print(" - диск (дисковая активность)")
-                print(" - hardware (полный отчет)")
-                print(" - выключи (завершить процесс)")
+                print(" - процессы (список процессов)(Системный модуль)")
+                print(" - cpu (загрузка процессора)(Системный модуль)")
+                print(" - ram (использование памяти)(Системный модуль)")
+                print(" - диск (дисковая активность)(Системный модуль)")
+                print(" - hardware (полный отчет)(Системный модуль)")
+                print(" - выключи (завершить процесс)(Системный модуль)")
+                print(" - браузер (авто-браузер)(Поисково-ответный модуль)")
+                print(" - поиск (авто-браузер)(Поисково-ответный модуль)")  
+                print(" - поиск по файлам (файловы поиск)(Файловый модуль)")
+                print(" - открой (открытие только jpg, jpe, png, txt, pdf)(Файловый модуль)")
                 print(" - exit (выход)\n")
                 continue
                 
             if not user_input:
                 continue
                 
-            result = system.execute(user_input)
+            result = core.execute(user_input)  
             if result:
                 print(result)
                 
         except KeyboardInterrupt:
             print("\nЗавершение работы...")
             break
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
